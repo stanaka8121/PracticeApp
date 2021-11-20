7587f186156e7ea8de9d536d4e934e8547d4e982
stanaka8121 <seijitanaka8121@gmail.com>
2021-11-20 20:34:50 +0900

    追加：Storyboard, Controller

---
+++ PracticeApp/Controller/SeijiTanakaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SeijiTanakaViewController.swift
+//  PracticeApp
+//
+//  Created by SeijiTanaka on 2021/11/20.
+//
+
+import UIKit
+
+class SeijiTanakaViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
